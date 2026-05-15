6f8d222b1c1a70b1ee7b88d89ecb634bf3917087
Majji Naga Pranathi <nagapranathimajji@gmail.com>
2026-05-15 21:52:27 +0530

    Update pdfData.js

--- src/data/pdfData.js
+++ src/data/pdfData.js
@@ -2,22 +2,22 @@ const pdfData = [
   {
     id: 1,
     title: "Tummy Tales (Rotating Menu)",
-    thumbnail: "/public/thumbnails/rotating-menu-thumbnail.png",
-    pdf: "/Tummy_Tales_Rotating_Menu.pdf",
+    thumbnail: "/thumbnails/rotating-menu-thumbnail.png",
+    pdf: "/pdfs/Tummy_Tales_Rotating_Menu.pdf",
   },
 
   {
     id: 2,
     title: "Tummy Tales (Meals Plan)",
-    thumbnail: "/meals-plan-thumbnail.png",
-    pdf: "/Tummy_Tales_Meals_Plan.pdf",
+    thumbnail: "//thumbnails/meals-plan-thumbnail.png",
+    pdf: "/pdfs/Tummy_Tales_Meals_Plan.pdf",
   },
 
   {
     id: 3,
     title: "Tummy Tales (Parent FAQ)",
-    thumbnail: "/parent-faq-thumbnail.png",
-    pdf: "/Tummy_Tales_Updated_Parent_FAQ.pdf",
+    thumbnail: "/thumbnails/parent-faq-thumbnail.png",
+    pdf: "/pdfs/Tummy_Tales_Updated_Parent_FAQ.pdf",
   },
 ]
 
